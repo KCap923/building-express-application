bfcb26148f50314e950df52dfd9e9032c0a67c91
Krystle Capuano <krystlecapuano@gmail.com>
2024-05-21 23:00:14 -0400

    "Middleware"

--- index.js
+++ index.js
@@ -2,23 +2,70 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.use(logger)
 
-// app.get("/", (req, res) => {
-//   res.send("Hello There!");
-// });
-
-// app.post("/post", (req, res) => {
-//   res.send("Checking POST request for user!");
-// });
-
-// HTML 
+// HTML *****************************************************************************************************
 app.set("view engine", "ejs")
 
 app.get("/", (req, res) => {
   res.render("index", {text: " & it's awesome!!!"})
 })
 
+// GET & POST request ***************************************************************************************
+app.get("/get", (req, res) => {
+  res.send("Checking GET request for user!");
+});
+
+app.post("/post", (req, res) => {
+  res.send("Checking POST request for user!");
+});
+// In Postman -----> Checking POST request for user!
+
+// Middleware *************************************************************************************************
+
+function logger(req, res, next) {
+  console.log("Log")
+  next()
+}
+//Terminal ------->
+// Server is running on port: 3000
+// /
+// /user
+// /get
+// Log
+
+// Exploring Response Actions ************************************************************************************
+
+
+
+
+
+
+
+
+
 
 app.listen(PORT, () => {
 console.log(`Server is running on port: ${PORT}`)
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.get("/users", (req, res) => {
+// res.send("User List")
+// });
+
+// app.get("/users/new", (req, res) => {
+// res.send("User New Form")
+// });
